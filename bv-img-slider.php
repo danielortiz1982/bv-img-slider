4b78e32e8811295c9fae6b92a8c2165687ca03c1
dortiz <dortiz@dortizs-MBP.lan1>
2021-04-02 11:33:29 -0400

    update to bootstrap 5

--- bv-img-slider.php
+++ bv-img-slider.php
@@ -4,7 +4,7 @@
 Plugin Name: BV Image Slider
 Plugin URI:  http://www.brandingverticals.com/
 Description: BV image slider is a dynamic Bootstrap image slider ~ Add shortcode - [bv-img-slider gallery_query="category-slug"]
-Version:     1.8
+Version:     2.0
 Author:      dortiz ~ BV Engineering
 Author URI:  http://www.brandingverticals.com/
 */
@@ -36,32 +36,34 @@ class BV_Img_Slider{
     // end of enqueue_scripts
 
     public function short_code_template($atts){
-
        extract(shortcode_atts(array('gallery_query' => 'bv-img-slider',), $atts));
        $args = array('posts_per_page' => '-1', 'post_type' => 'bv_img_slider', 'tax_query' => array(array('taxonomy' => 'bv_img_slider_taxonomy', 'field'    => 'slug', 'terms' => $gallery_query)));
        $the_query = new WP_Query( $args );
-
-       $return_string = '<div id="'.$gallery_query.'" class="carousel slide" data-bs-ride="carousel">';
-       $return_string .= '<ol class="carousel-indicators">';
+       $return_string = '<div id="'.$gallery_query.'" class="carousel carousel-dark slide" data-bs-ride="carousel">';
+       // Start Indicators
+       $return_string .= '<div class="carousel-indicators">';
        while ( $the_query->have_posts() ) : $the_query->the_post();
-        $return_string .= '<li data-bs-target="#'.$gallery_query.'" data-bs-slide-to="0" class="slide';
-        if ($the_query->current_post == 0) $return_string .= ' active';
-        $return_string .= '"></li>';
+        $return_string .= '<button type="button" data-bs-target="#'.$gallery_query.'" data-bs-slide-to="'.$the_query->current_post.'" aria-current="true" aria-label="Slide '.$the_query->current_post.'"';
+        if ($the_query->current_post == 0) $return_string .= ' class="active"';
+        $return_string .= '</button>';
      endwhile;
-     $return_string .= '</ol>';
+     $return_string .= '</div>';
      wp_reset_query();
-
+     // end Indicators
+       // start carousel-inner 
        $return_string .= '<div class="carousel-inner">';
        while ( $the_query->have_posts() ) : $the_query->the_post();
         $return_string .= '<div class="carousel-item';
         if ($the_query->current_post == 0) $return_string .= ' active';
         $return_string .= '">';
-        $return_string .= '<img class="img-fluid" src="' . get_the_post_thumbnail_url() . '" /></div>';
+        $return_string .= '<img class="img-fluid" src="' . get_the_post_thumbnail_url() . '" />';
+        $return_string .= '</div>';
+
     endwhile;
-       $return_string .= '<a class="carousel-control-next" href="#'.$gallery_query.'" role="button" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></a>';
-       $return_string .= '  <a class="carousel-control-prev" href="#'.$gallery_query.'" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></a>';
-       $return_string .= '</div>';
-       $return_string .= '</div>';
+    // end carousel-inner
+      $return_string .= '<button class="carousel-control-prev" type="button" data-bs-target="#'.$gallery_query.'" data-bs-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="visually-hidden">Previous</span></button>';
+      $return_string .= '<button class="carousel-control-next" type="button" data-bs-target="#'.$gallery_query.'" data-bs-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="visually-hidden">Next</span></button>';
+      $return_string .= '</div>';
        wp_reset_query();
        return $return_string;
     }
